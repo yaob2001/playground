38bd8f62ee8afa9c84960ddfa2f4bf91e861868f
Bin Yao <yaob2001@gmail.com>
2017-05-16 16:46:11 -0400

    BY: PG-1 add more analyzer and test

--- eventlogs/src/main/java/org/yaobin/play/generator/IEventGenerator.java
+++ eventlogs/src/main/java/org/yaobin/play/generator/IEventGenerator.java
@@ -1,7 +1,13 @@
 package org.yaobin.play.generator;
 
+import org.yaobin.play.data.EventDetail;
+
+import java.time.Instant;
+import java.util.List;
+
 /**
  * Created by Bin on 5/12/2017.
  */
 public interface IEventGenerator {
+    List<EventDetail> generateEvents(Instant startTime, long durationInMills, int numOfUsers, int numOfEvents);
 }
